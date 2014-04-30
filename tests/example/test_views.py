9db8b02f63c6a1dea9403d4475491aed681cb737
Nick Charlton <hello@nickcharlton.net>
2014-04-30 16:39:15 +0100

    Adds tests for the example Blueprint.

---
+++ tests/example/test_views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.testing import TestCase
+
+from app import app, db
+
+
+class ExampleViewsTestCase(TestCase):
+    def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        app.config['TESTING'] = True
+
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_index(self):
+        response = self.client.get('/')
+
+        self.assert_200(response)
+        self.assert_template_used('example/index.html')
+        assert '<p>Hello World!</p>' in response.data
