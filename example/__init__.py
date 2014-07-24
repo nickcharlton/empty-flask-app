a9e3b26cff36afd68579302d9f2b9103c31336f5
Nick Charlton <hello@nickcharlton.net>
2014-07-24 12:53:50 +0100

    Reinstates the default error handlers.

--- example/__init__.py
+++ example/__init__.py
@@ -12,4 +12,11 @@ from .views.static import static
 app.register_blueprint(static)
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
